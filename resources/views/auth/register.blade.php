2031096ee50095af119a4c0a41a6c9713dfae4c3
Edward Simarmata <edwardsaragih97@gmail.com>
2018-11-02 10:12:26 +0700

    finishing register and page of register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,15 +5,16 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+                <div class="card-header justify-contenct-center">{{ __('Register') }}</div>
 
                 <div class="card-body">
                    
 
                          <div class="form-group row mb-0">
+
+                            <h4><center>Choose wanna register with :</center></h4>
                             <div class="col-md-6 offset-md-4">
                                  
-                            
                                 <button name="data" type="button" onclick="myFunction()">Merchant</button>
                             
 
@@ -22,7 +23,77 @@
                             </div>
                         </div>
 
-                        <div id="registerMerchant">
+                        <div id="registerMerchant" style="display:none;" class="answer_list">
+                            This is Register Merchant
+                            <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <input type="hidden" value="merchant" name="role"/>
+
+                        <div class="form-group row">
+                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control{{ $errors->has('username') ? ' is-invalid' : '' }}" name="username" value="{{ old('username') }}" required autofocus>
+
+                                @if ($errors->has('username'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('username') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                      
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password-confirm" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Register') }}
+                                </button>
+                            </div>
+                        </div>
+                        </form>
+
+                        </div>
+
+                        <div id="registerCustomer" style="display:none;" class="answer_list">
                         <form method="POST" action="{{ route('register') }}">
                         @csrf
 
@@ -88,10 +159,6 @@
                             </div>
                         </div>
                         </form>
-                        </div>
-
-                        <div id="registerCustomer">
-                            This is register Customer!!
 
                         </div>
 
@@ -108,22 +175,19 @@
 function myFunction() {
     var x = document.getElementById("registerMerchant");
     var y = document.getElementById("registerCustomer");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    }
-    else{
-    x.style.display = "none"
-    }
+
+    x.style.display = "block";
+    y.style.display = "none";
 }
 
+
 function myFunctionCustomer() {
-    var x = document.getElementById("registerCustomer");
-    var y = document.getElementById("registerMerchant");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
+    var x = document.getElementById("registerMerchant");
+    var y = document.getElementById("registerCustomer");
+
+    x.style.display = "none";
+    y.style.display = "block";
+    
 }
 </script>
 @endsection
