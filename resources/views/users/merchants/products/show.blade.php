dfeedaa5c195dcf1cbf9ce3942b7bfc90a1ee6d2
Eko Simanjuntak <ecojuntak@gmail.com>
2018-11-15 10:08:21 +0700

    add recomended product section

--- resources/views/users/merchants/products/show.blade.php
+++ resources/views/users/merchants/products/show.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 <div class="col-md-12">
   <div class="row">
-    <div class="col-md-10">
+    <div class="col-md-12">
       <div class="card globalcard">
         <div class="card-body">
           <div class="row">
-            <div class="col-md-4">
+            <div class="col-md-6">
               <div class="detailproduct">
                 <div id="myCarousel" class="carousel slide " data-ride="carousel">
                   <!-- Wrapper for slides -->
@@ -68,14 +68,236 @@
         </div>
       </div>
     </div>
-    <div class="col-md-2">
-      <div class="card globalcard">
-        <div class="card-body">
-          asd
+  </div>
+</div>
+
+<div class="row">
+
+  <!--Card Swipe-->
+  <div class="col-md-12">
+    <div class="card globalcard">
+      <div class="card-header">
+        <h6>Produk Rekomendasi</h6>
+      </div>
+      <div class="card-body">
+        <div id="carouselExampleIndicators" class="carousel slide mt-0" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <div class="row">
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-33.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-44.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-44.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div class="carousel-item">
+              <div class="row">
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="col-md-2">
+                  <div class="card product">
+                    <div class="imgwrapper">
+                      <img src="img/product-img/product-22.jpg" alt="Card image cap">
+                    </div>
+
+                    <div class="card-body">
+                      <p class="card-title productname">Ulos Ragihotang asdjahslkd</p>
+                      <h6 style="color: #ff5205"> Rp 2.400.000</h6>
+                      <p class="card-text float-right">
+                        <small class="text-muted">Balige</small>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev"
+             style="margin-left: -10px">
+            <span class="carousel-control-prev-icon" aria-hidden="true" style="background-color: black;
+    padding: 10px;
+    height: 50px;
+    width: 30px;"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true" style="background-color: black;
+    padding: 10px;
+    height: 50px;
+    width: 30px;"></span>
+            <span class="sr-only">Next</span>
+          </a>
         </div>
       </div>
     </div>
+
   </div>
+  <!--Card Swipe End-->
 </div>
 @endsection
 
