859ac81bbd1cfb149572d566caeff08ae8f0ce0e
Eko Simanjuntak <ecojuntak@gmail.com>
2018-11-28 21:33:43 +0700

    unfinish blog fixing

---
+++ resources/js/components/blogs/BlogList.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="blog-wrapper">
+    <div class="container">
+      <div class="row">
+        <div class="col-12 col-lg-6">
+          <div class="single-blog-area mb-50">
+            <img src="/images/1542352749ragihotang3.jpeg" alt>
+            <!-- Post Title -->
+            <div class="post-title">
+              <a href="#">Filosofi Ulos</a>
+            </div>
+            <!-- Hover Content -->
+            <div class="hover-content">
+              <!-- Post Title -->
+              <div class="hover-post-title">
+                <a href="#">Filosofi Ulos</a>
+              </div>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis
+                fringilla tortor. Phasellus purus dignissim convallis.
+              </p>
+              <a href="single-blog.html">
+                Continue reading
+                <i class="fa fa-angle-right"></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      blogs: []
+    };
+  },
+  methods: {
+    getBlogs() {
+      window.axios.get('/get-blogs').then(res => {
+        this.blogs = res.data
+        console.log(this.blogs)
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  },
+  created() {
+    this.getBlogs();
+  }
+};
+</script>
+
+<style>
+</style>
