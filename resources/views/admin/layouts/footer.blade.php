9ae2560b510192328da4453ac4d1fb80d4d115e9
Eko Simanjuntak <ecojuntak@gmail.com>
2018-11-29 09:16:24 +0700

    change admin template

--- resources/views/admin/layouts/footer.blade.php
+++
@@ -1,9 +0,0 @@
-<footer class="site-footer">
-    <div class="footer-inner bg-white">
-        <div class="row">
-            <div class="col-sm-6">
-                Copyright &copy; 2018 <a href="{{ url('/')}}">UlosZone</a>
-            </div>
-        </div>
-    </div>
-</footer>
