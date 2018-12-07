2e667c8115f0b40eb38ab75ffa646579262d04cc
Edward Simarmata <edwardsaragih97@gmail.com>
2018-12-07 10:30:14 +0700

    on progress for data graphics per date

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Transaction;
 use DB;
+use Carbon\Carbon;
 
 class AdminController extends Controller
 {
@@ -16,6 +17,21 @@ class AdminController extends Controller
      
     $countCustomer = DB::table('model_has_roles')->where('role_id', 3)->count();
 
+    // $dataPerDates = collect();
+    // foreach( range( -6, 0) AS $i){
+    //   $date = Carbon::now()->addDays( $i )->format('Y-m-d');
+    //   $dataPerDates->put( $date, 0);
+    // }
+
+    // $OrderperDate = Transaction::where( 'created_at', '>=', $dataPerDates->keys()->first() )
+    //                           ->groupBy( 'date' )
+    //                           ->orderBy( 'date' )
+    //                           ->get( [
+    //                               DB::raw( 'DATE( created_at ) as date' ),
+    //                               DB::raw( 'COUNT( * ) as "count"' )
+    //                           ] )
+    //                           ->pluck( 'count', 'date' );
+    // dd($OrderperDate);
     return view('admin.index')
     ->with('countOrder', $countOrder)
     ->with('countMerchant', $countMerchant)
