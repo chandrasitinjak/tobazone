753579830263b77788f41c70ecf3733a5118d97b
Palti28 <paltigcsinaga@gmail.com>
2018-12-07 08:31:55 +0700

    dummy blog footer fix

--- resources/js/components/blogs/BlogList.vue
+++ resources/js/components/blogs/BlogList.vue
@@ -1,32 +1,121 @@
 <template>
-  <div class="blog-wrapper" style="margin-top: 100px">
-    <div class="container">
-      <div class="row">
-        <div class="col-12 col-lg-6">
-          <div class="single-blog-area mb-50">
-            <img src="/images/1542352749ragihotang3.jpeg" alt>
-            <!-- Post Title -->
-            <div class="post-title">
-              <a href="#">Filosofi Ulos</a>
-            </div>
-            <!-- Hover Content -->
-            <div class="hover-content">
-              <!-- Post Title -->
-              <div class="hover-post-title">
-                <a href="#">Filosofi Ulos</a>
+
+  <div class="container" style="margin-top: 100px; ">
+    <div class="card globalcard blog" style="min-height: 400px">
+
+      <div class="card-body">
+        <div class="row">
+        <div class="card-deck">
+          <div class="col-4">
+            <div class="card blogcard " style="border: none">
+              <div class="card-header blogpict" style="background-image: url('/images/banners/ulos modif.jpg');">
+                <!--<img src="/images/banners/ulos modif.jpg" alt>-->
               </div>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis
-                fringilla tortor. Phasellus purus dignissim convallis.
-              </p>
-              <a href="single-blog.html">
-                Continue reading
-                <i class="fa fa-angle-right"></i>
-              </a>
+              <div class="card-body px-0">
+
+                <!-- Post Title -->
+                <div class="h4">
+                  <a href="#">
+                  <h4>Filosofi Ulos</h4>
+                  </a>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis
+                  fringilla tortor. Phasellus purus dignissim convallis.
+                </p>
+                <a href="#">
+                  Continue reading
+                  <i class="fa fa-angle-right"></i>
+                </a>
+
+                </div>
+              </div>
+            </div>
+          <div class="col-4">
+            <div class="card blogcard" style="border: none">
+              <div class="card-header blogpict " style="background-image: url('/images/banners/alattenun.jpg')">
+                <!--<img src="/images/banners/ulos modif.jpg" alt>-->
+              </div>
+              <div class="card-body px-0">
+
+                <!-- Post Title -->
+                <div class="h4">
+                  <a href="#">
+                    <h4>Macam Pembuatan Ulos</h4>
+                  </a>
+                </div>
+                <p>
+                  ATBM (Alat Tenun Bukan Mesin) adalah alat pembuat kain dasar untuk dijadikan sebagai kain tenun tradisional. ATBM terdiri dari rangka kayu yang gerakan mekaniknya dilakukan oleh ...
+                </p>
+                <a href="#">
+                  Continue reading
+                  <i class="fa fa-angle-right"></i>
+                </a>
+
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card blogcard" style="border: none">
+              <div class="card-header blogpict" style="background-image: url('/images/banners/ulos modif.jpg');">
+                <!--<img src="/images/banners/ulos modif.jpg" alt>-->
+              </div>
+              <div class="card-body px-0">
+
+                <!-- Post Title -->
+                <div class="h4">
+                  <a href="#">
+                    <h4>Filosofi Ulos</h4>
+                  </a>
+                </div>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce enim nulla, mollis eu metus in, sagittis
+                  fringilla tortor. Phasellus purus dignissim convallis.
+                </p>
+                <a href="#">
+                  Continue reading
+                  <i class="fa fa-angle-right"></i>
+                </a>
+
+              </div>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card blogcard" style="border: none">
+              <div class="card-header blogpict" style="background-image: url('/images/banners/sindo_ulos_bandung.jpg');">
+                <!--<img src="/images/banners/ulos modif.jpg" alt>-->
+              </div>
+              <div class="card-body px-0">
+
+                <!-- Post Title -->
+                <div class="h4">
+                  <a href="#">
+                    <h4>Tips Merawat Tenun Ulos</h4>
+                  </a>
+                </div>
+                <p>
+                  adu padan kain songket membuat apa saja terlihat mewah.
+                  Kain jenis ini ditenun dengan menggunakan benang emas dan perak.
+                  Beberapa yang lain menggunakan benang sutera, benang sulam, atau
+                  benang katun berwarna sebagai bahan dasar. Bahannya yang istimewa dan
+                  pembuatannya yang rumit membuat kain asal Sumatera ini perlu perawatan khusus...
+
+                </p>
+                <a href="#">
+                  Continue reading
+                  <i class="fa fa-angle-right"></i>
+                </a>
+
+              </div>
             </div>
           </div>
         </div>
+          </div>
+        </div>
       </div>
+
+
+
     </div>
   </div>
 </template>
--- resources/js/components/search/SearchProduct.vue
+++ resources/js/components/search/SearchProduct.vue
@@ -7,12 +7,12 @@
             <ul class="navbar-nav mr-auto">
               <li class="nav-item dropdown">
                 <a
-                  class="nav-link dropdown-toggle"
-                  id="navbarDropdown"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
+                    class="nav-link dropdown-toggle"
+                    id="navbarDropdown"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
                 >Kategori</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown">
                   <a class="dropdown-item" @click="filterProductByCategory('ATBM')">Alat Tenun Bukan Mesin</a>
@@ -24,12 +24,12 @@
               </li>
               <li class="nav-item dropdown">
                 <a
-                  class="nav-link dropdown-toggle"
-                  id="navbarDropdown1"
-                  role="button"
-                  data-toggle="dropdown"
-                  aria-haspopup="true"
-                  aria-expanded="false"
+                    class="nav-link dropdown-toggle"
+                    id="navbarDropdown1"
+                    role="button"
+                    data-toggle="dropdown"
+                    aria-haspopup="true"
+                    aria-expanded="false"
                 >Urutkan Berdasarkan</a>
                 <div class="dropdown-menu" aria-labelledby="navbarDropdown1">
                   <a class="dropdown-item" @click="sortProductByName('asc')">Nama A-Z</a>
@@ -41,73 +41,73 @@
             </ul>
             <div class="form-inline my-2 my-lg-0">
               <input
-                class="form-control form-control-sm mr-sm-2"
-                type="search"
-                placeholder="Filter Nama"
-                aria-label="Search"
-                v-model="searchName"
-                @input="filterProductByName"
+                  class="form-control form-control-sm mr-sm-2"
+                  type="search"
+                  placeholder="Filter Nama"
+                  aria-label="Search"
+                  v-model="searchName"
+                  @input="filterProductByName"
               >
             </div>
           </div>
         </nav>
       </div>
       <div class="card-body">
-      <div class="" v-if="products.length>0">
-        <div class="row" >
-          <div v-for="product in products" class="col-lg-2 col-md-2 col-sm-2 px-1">
-            <div class="card products">
-              <a :href="'/products/' + product.id">
-                <div class="imgwrapper">
-                  <img :src="'/images/' + JSON.parse(product.images)[0]" alt="Card image cap">
+        <div class="" v-if="products.length>0">
+          <div class="row">
+            <div v-for="product in products" class="col-lg-2 col-md-2 col-sm-2 px-1">
+              <div class="card products">
+                <a :href="'/products/' + product.id">
+                  <div class="imgwrapper">
+                    <img :src="'/images/' + JSON.parse(product.images)[0]" alt="Card image cap">
+                  </div>
+                </a>
+                <div class="card-body">
+                  <p class="card-title productname">{{ product.name }}</p>
+                  <h6 style="color: #ff5205">Rp {{formatPrice( product.price )}}</h6>
+                  <p class="card-text float-right">
+                    <small class="text-muted"> {{ product.merchant.profile.name }}</small>
+                  </p>
                 </div>
-              </a>
-              <div class="card-body">
-                <p class="card-title productname">{{ product.name }}</p>
-                <h6 style="color: #ff5205">Rp {{formatPrice( product.price )}}</h6>
-                <p class="card-text float-right">
-                  <small class="text-muted"> {{ product.merchant.profile.name }}</small>
-                </p>
               </div>
             </div>
           </div>
+          <div class="row">
+            <!-- Pagination -->
+            <nav aria-label="navigation">
+              <ul class="pagination mt-50 mb-50">
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    <i class="fa fa-angle-left"></i>
+                  </a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">1</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">2</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">3</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">...</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">21</a>
+                </li>
+                <li class="page-item">
+                  <a class="page-link" href="#">
+                    <i class="fa fa-angle-right"></i>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
         </div>
-        <div class="row">
-          <!-- Pagination -->
-          <nav aria-label="navigation">
-            <ul class="pagination mt-50 mb-50">
-              <li class="page-item">
-                <a class="page-link" href="#">
-                  <i class="fa fa-angle-left"></i>
-                </a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">1</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">2</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">3</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">...</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">21</a>
-              </li>
-              <li class="page-item">
-                <a class="page-link" href="#">
-                  <i class="fa fa-angle-right"></i>
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
 
         <div v-else class="col-12 center center-block align-center text-center">
-          <img src="/images/assets/search_result_empty.png" style="height: 120px; border: none; opacity: 0.5" />
+          <img src="/images/assets/search_result_empty.png" style="height: 120px; border: none; opacity: 0.5"/>
           <p class="text font-bold">
             <br>
             Oops, produk tidak ditemukan :(
