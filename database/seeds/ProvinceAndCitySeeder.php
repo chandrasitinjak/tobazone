03b88eb45df5d56baf9eb97637b3d8e26340e2a7
chandrasitinjak <dedichandra926@gmail.com>
2020-02-27 21:17:53 +0700

    changes in city seeder

--- database/seeds/ProvinceAndCitySeeder.php
+++ database/seeds/ProvinceAndCitySeeder.php
@@ -30,13 +30,18 @@ class ProvinceAndCitySeeder extends Seeder
         $string = file_get_contents("database/seeds/city.json");
         $cities = json_decode($string);
         
+
+        $id_city = 1;
         foreach($cities as $city) {
             City::create([
+                "id" => $id_city,
                 "province_id" => $city->province_id,
                 "type" => $city->type,
                 "name" => $city->city_name,
                 "postal_code" => $city->postal_code,
             ]);
+
+            $id_city++;
         }
     }
 }
