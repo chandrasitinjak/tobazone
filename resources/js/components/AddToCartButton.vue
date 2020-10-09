e0e54cabe51789f2998a8fb7d7ce948b0f29bd90
= <=>
2020-10-10 01:35:14 +0700

    Responsive User Product

--- resources/js/components/AddToCartButton.vue
+++ resources/js/components/AddToCartButton.vue
@@ -1,10 +1,32 @@
 <template>
-  
+
     <div class="cart-fav-box d-flex align-items-center mt-4">
-      <button v-on:click="addToCart()" class="btn essence-btn">Tambah ke keranjang</button>
+      <button v-on:click="addToCart()" class="btn essence-btn w-100">Tambah ke keranjang</button>
     </div>
-  
+
 </template>
+
+<style scoped>
+button{
+    font-size: 13px;
+}
+@media (min-width:219px) and (max-width: 449px){
+
+}
+@media (min-width:450px) and (max-width: 767px){
+
+}
+@media (min-width:768px) and (max-width: 990px){
+    button{
+        font-size: 10px;
+    }
+}
+@media (min-width:991px) and (max-width: 1230px){
+    button{
+        font-size: 10px;
+    }
+}
+</style>
 
 <script>
 import EventBus from "../eventBus";
@@ -29,7 +51,7 @@ export default {
       window.axios.post("/api/carts", payload).then(res => {
         this.emitEvent(res.data);
       });
-      
+
       //pindah ke halaman keranjang
       window.location = "/carts"
     },
