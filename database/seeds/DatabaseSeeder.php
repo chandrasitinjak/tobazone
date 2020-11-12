cf37af64d95458e70bb7cee874c9ee1de3fc0d39
Andreas Tampubolon <andreasjr61@gmail.com>
2020-11-12 08:48:07 +0700

    add new branch

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
         $this->call(UsersTableSeeder::class);
         $this->call(RoleAndPermissionSeeder::class);
         $this->call(ProvinceAndCitySeeder::class);
+
     }
 }
