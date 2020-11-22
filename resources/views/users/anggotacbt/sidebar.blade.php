85331c1bb2f4a2746ed18861d9587c80f9e9bc18
ucokpel1112 <kristopelltoruan11@gmail.com>
2020-11-23 03:12:44 +0700

    Integrasi CBT side, untuk bagian nampilin komunitas dan bergabung Frontend+backend

--- resources/views/users/anggotacbt/sidebar.blade.php
+++ resources/views/users/anggotacbt/sidebar.blade.php
@@ -25,7 +25,7 @@
                 <!-- Add icons to the links using the .nav-icon class
                      with font-awesome or any other icon font library -->
                 <li class="nav-item has-treeview ">
-                    <a href="#" class="nav-link ">
+                    <a href="{{url('cbt.dashboard')}}" class="nav-link ">
                         <i class="nav-icon fa fa-tachometer-alt"></i>
                         <p>
                             Dashboard
@@ -42,7 +42,7 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="{{url('anggotacbt/komunitas')}}" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Daftar Komunitas </p>
                             </a>
