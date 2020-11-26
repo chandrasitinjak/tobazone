4ed5a784d7de748af4ab016db16bad84c2e636f1
Gita <gita.nadapdap@gmail.com>
2020-11-26 20:32:42 +0700

    FE uploadBayar

---
+++ resources/views/payment/upload-bukti-pembayaran.blade.php
@@ -0,0 +1,9 @@
+@extends('users.layouts.app') 
+@section('title') {{ "Konfirmasi Pembayaran" }}
+@endsection
+ 
+@section('content')
+<div id="payment-confirmation">
+<payment-confirmation :user-id="{{ Auth::user()->id }}" :transaction-id="{{ Request::segment('4') }}" />
+</div>
+@endsection
