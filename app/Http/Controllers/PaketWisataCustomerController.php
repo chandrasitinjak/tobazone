47383b741aa305e906a79b900cb75220436e838c
bennipanbers224 <benni.pernadi@gmail.com>
2020-11-28 15:01:11 +0700

    membuat pemesanan paket wisata

--- app/Http/Controllers/PaketWisataCustomerController.php
+++ app/Http/Controllers/PaketWisataCustomerController.php
@@ -5,8 +5,11 @@ namespace App\Http\Controllers;
 use App\Comment;
 use App\IncludedNotIncluded;
 use App\PaketWisata;
+use App\Pemesanan;
 use App\Sesi;
 use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
 
 class PaketWisataCustomerController extends Controller
 {
@@ -43,6 +46,21 @@ class PaketWisataCustomerController extends Controller
         $paket_lain = PaketWisata::where([['status', 1],['kabupaten_id',4],['id_paket','!=',$id_paket]])->orderBy('created_at', 'DESC')->first();
 //        dd($paket_lain);
         return view('paket-wisata.detail_paket', compact('counts','paket_lain','comments','paket', 'hotel', 'sesi', 'includeds', 'not_includeds'));
+
+    }
+    public function bookingPaket(Request $request, $id_paket){
+        $result = DB::table('sesi')
+            ->where('paket_id',$id_paket)
+            ->pluck('id_sesi')
+            ->first();
+        $paketWisata = new Pemesanan();
+        $paketWisata->jumlah_peserta = $request->jumlah_orang;
+        $paketWisata->user_id = Auth::user()->id;
+        $paketWisata->status = 1;
+        $paketWisata->pesan = $request->pesan;
+        $paketWisata->sesi_id = $result;
+        $paketWisata->save();
+        return redirect()->back();
 
     }
 }
