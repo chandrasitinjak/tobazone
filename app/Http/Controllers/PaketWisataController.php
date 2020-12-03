7a2b59319a0c33ccb5abb6af912d668e0f36aa67
chandrasitinjak <dedichandra926@gmail.com>
2020-12-04 06:52:48 +0700

    dedi - BE_PencarianPaketWisata

--- app/Http/Controllers/PaketWisataController.php
+++ app/Http/Controllers/PaketWisataController.php
@@ -42,6 +42,32 @@ class PaketWisataController extends Controller
         return view('paket-wisata.paketwisata',compact('paket', 'jenis', 'kabupaten','paket_lainnya'));
     }
 
+    public function indexFilter(Request $request)
+    {
+        $kabupaten = Kabupaten::all();
+        $id_kab = $request->kabupaten;
+        $jeniss = $request->jenis;
+        $jenis = DB::table('paket_wisata')->select('jenis_paket')->groupBy('jenis_paket')->get();
+        $pakets = paketWisata::where('status', 1)->orderBy('created_at', 'DESC')->get();
+        $paket_lainnya = $pakets->take(3);
+
+        if ($request->jenis == 'Tipe/Jenis Perjalanan') {
+            if ($request->kabupaten == 'Kabupaten') {
+                $paket = paketWisata::where('status', 1)->orderBy('created_at', 'DESC')->paginate(10);
+            } else {
+                $paket = paketWisata::where([['kabupaten_id', $id_kab], ['status', 1]])->orderBy('created_at', 'DESC')->paginate(10);
+            }
+        } else {
+            if ($request->kabupaten == 'Kabupaten') {
+                $paket = paketWisata::where([['jenis_paket', 'LIKE', '%' . $request->jenis . '%'], ['status', 1]])->orderBy('created_at', 'DESC')->paginate(10);
+            } else {
+                $paket = paketWisata::where([['status', 1], ['kabupaten_id', $id_kab], ['jenis_paket', 'LIKE', '%' . $request->jenis . '%']])->orderBy('created_at', 'DESC')->paginate(10);
+            }
+        }
+
+        return view('paket-wisata.paketwisata',compact('paket', 'jenis', 'kabupaten','paket_lainnya', 'jeniss', 'id_kab'));
+    }
+
     public function show($id_paket)
     {
         $paket = PaketWisata::where('id_paket', $id_paket)->with(['getKabupaten', 'getIncludedNotIncluded', 'getPaketLayanan'])->first();
