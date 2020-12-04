703cdbdf817ee7d09d78ac9e11dbb9f75361241e
helmuth12081999 <helmuthsimon123@gmail.com>
2020-12-04 16:58:56 +0700

    perbaikan list paket wisata

--- resources/views/paket-wisata/paketwisata.blade.php
+++ resources/views/paket-wisata/paketwisata.blade.php
@@ -115,7 +115,7 @@
            <form action="{{route('paket.filter')}}" method="post">
            @csrf
             <ul class="navbar-nav mr-auto mt-2 mt-lg-0" style="position : relative; float : right">
-              <li class="mr-1 ml-auto">  
+              <li class="mr-1 ml-auto">
                 <select name="kabupaten" style="width: 90%;padding: 14px 12px;
                                                 border: none;
                                                 border-radius: 5px;
@@ -124,25 +124,25 @@
                         <option data-display="Kabupaten">Kabupaten</option>
                         @foreach($kabupaten as $row)
                             <option value="{{$row->id_kabupaten}}" data-display="{{$row->nama_kabupaten}}"
-                            {{(isset($id_kab)&&($id_kab==$row->id_kabupaten))?'selected':null}}> {{$row->nama_kabupaten}} </option>                                                            
-                        @endforeach                               
-                </select>                                
+                            {{(isset($id_kab)&&($id_kab==$row->id_kabupaten))?'selected':null}}> {{$row->nama_kabupaten}} </option>
+                        @endforeach
+                </select>
               </li>
               <li class="mr-1 ml-auto">
-              
+
                 <select name="jenis" style="width: 90%;padding: 14px 22px;
                                             border: none;
                                             border-radius: 5px;
                                             color: black;
                                             background-color: #d9d9d9;">
-                    <option data-display="Tipe/Jenis Perjalanan"> Tipe/Jenis Perjalanan  </option>      
-                        @foreach($jenis as $row)                                                    
-                            <option value="{{$row->jenis_paket}}" data-display="{{$row->jenis_paket}}" 
-                            {{(isset($jeniss)&&(strcmp($jeniss,$row->jenis_paket)==0)?'selected':null)}}>                       
-                            {{$row->jenis_paket}}</option>                            
-                                                                
-                        @endforeach                           
-                </select>                             
+                    <option data-display="Tipe/Jenis Perjalanan"> Tipe/Jenis Perjalanan  </option>
+                        @foreach($jenis as $row)
+                            <option value="{{$row->jenis_paket}}" data-display="{{$row->jenis_paket}}"
+                            {{(isset($jeniss)&&(strcmp($jeniss,$row->jenis_paket)==0)?'selected':null)}}>
+                            {{$row->jenis_paket}}</option>
+
+                        @endforeach
+                </select>
               </li>
 
               <li class="mr-1 ml-auto">
@@ -152,13 +152,13 @@
                                             color: black;
                                             background-color: #d9d9d9;;"><i class="fa fa-filter" aria-hidden="true"></i> Filter</button>
               </li>
-              
-            </ul>    
-            </form>       
+
+            </ul>
+            </form>
           </div>
         </nav>
                 </div>
-            </div>        
+            </div>
         </div>
 
         <div class="card-body">
@@ -170,7 +170,7 @@
                                 <a href="{{route('paket.detail',$row->id_paket)}}">
 
                                     <img class="card-img-top"
-                                         src="{{ asset("/images/carousels/1588556676inspirasi_2.png" )}}" alt="Card image cap" style="height: 185px">
+                                         src="{{ asset("/storage/img/paket/".$row->gambar )}}" alt="Card image cap" style="height: 185px">
 
                                     <div class="card-body">
                                         <h7 class="card-title" style="font-weight: bold">{{$row->nama_paket}} ( <i class="fa fa-clock-o"></i> &nbsp;{{$row->durasi}} )</h7>
@@ -191,13 +191,13 @@
                                 </a>
                             </div>
                         </div>
-                        @empty 
+                        @empty
                         <div class="col-12 center center-block align-center text-center">
                         <img src="/images/assets/search_result_empty.png" style="height: 120px; border: none; opacity: 0.5"/>
                         <p class="text font-bold">
                             <br>
                             Paket Wisata Belum Ada
-                        </p>                        
+                        </p>
                         </div>
                         @endforelse
                 </div>
