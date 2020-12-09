73397a32a65ba7c0a5765ddd6bbad11c4ad7ddd7
Andreas Tampubolon <andreasjr61@gmail.com>
2020-12-09 19:27:43 +0700

    update navbar informasi pariwisata

--- resources/views/users/informasi/objekWisata/detail/objekWisata.blade.php
+++ resources/views/users/informasi/objekWisata/detail/objekWisata.blade.php
@@ -1,4 +1,4 @@
-@extends('users.informasi.app')
+@extends('users.layouts.app')
 @section('title') {{ "Detail Objek Wisata" }}
 @endsection
 
--- resources/views/users/informasi/transportasi/detail/transportasi.blade.php
+++ resources/views/users/informasi/transportasi/detail/transportasi.blade.php
@@ -1,4 +1,4 @@
-@extends('users.informasi.app')
+@extends('users.layouts.app')
 @section('title') {{ "Detail Transportasi" }}
 @endsection
 
