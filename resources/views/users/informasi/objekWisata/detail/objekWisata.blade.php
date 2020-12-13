f9cad1c3475fc70b256e64d62a9dda5c0e53da3d
Andreas Tampubolon <andreasjr61@gmail.com>
2020-12-13 20:42:31 +0700

    fix bug for image detail by kristopel

--- resources/views/users/informasi/objekWisata/detail/objekWisata.blade.php
+++ resources/views/users/informasi/objekWisata/detail/objekWisata.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
     <div class="jumbutron mt-50">
-        <div class="jumbotron text-white jumbotron-image shadow"  style="height:500px;background-image: url(<?= $objekWisata->foto ?>);background-size:2000px 1000px ;background-repeat: no-repeat">
+        <div class="jumbotron text-white jumbotron-image shadow"  style="height:500px;background-image: url({{'/Kab/information/ObjekWisata/'.$objekWisata->foto}});background-size:2000px 1000px ;background-repeat: no-repeat">
             <h1 class="mb-4 text-white">
                 <span class="badge badge-light">{{$objekWisata->nama_objek_wisata}}</span>
             </h1>
