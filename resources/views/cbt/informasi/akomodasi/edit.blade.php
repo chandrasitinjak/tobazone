2eef1c90ba9d1437a9f4e77ca8dd1f5c907efaec
Andreas Tampubolon <andreasjr61@gmail.com>
2020-12-14 16:58:51 +0700

    add view informasi CBT

--- resources/views/cbt/informasi/akomodasi/edit.blade.php
+++ resources/views/cbt/informasi/akomodasi/edit.blade.php
@@ -1,7 +1,5 @@
 @extends('users.anggotacbt.app')
 @section('content')
-
-@section('content')
         <div class="content-header">
             <div class="container-fluid">
                 <div class="row mb-2">
--- resources/views/cbt/informasi/objekwisata/index.blade.php
+++ resources/views/cbt/informasi/objekwisata/index.blade.php
@@ -162,6 +162,7 @@
                                             <td><form action="{{ route('objekwisata.destroy', $objekWisata->id) }}" method="POST">
                                                     @csrf
                                                     <input type="hidden" name="_method" value="DELETE">
+                                                    <a href="{{ route('objekwisata.show', $objekWisata->id) }}" class="btn btn-success btn-sm"><i class="fa fa-eye"></i></a>
                                                     <a href="{{ route('objekwisata.edit', $objekWisata->id) }}" class="btn btn-warning btn-sm"><i class="fa fa-edit"></i></a>
                                                     <button class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button>
                                                 </form></td>
