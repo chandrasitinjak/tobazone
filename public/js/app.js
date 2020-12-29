87aa363ac5cd7b65115987f388dac09c8f2b0a4d
Andreas Tampubolon <andreasjr61@gmail.com>
2020-12-29 15:57:31 +0700

    update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -112542,8 +112542,8 @@ var EventBus = new vue__WEBPACK_IMPORTED_MODULE_0___default.a();
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Project\Tobazone\Finish\tobazone\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Project\Tobazone\Finish\tobazone\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\xampp\htdocs\tobazone-v3\tobazone\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\xampp\htdocs\tobazone-v3\tobazone\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
