21469584e2b3a1f0872333deee012ce37a17cde4
helmuth12081999 <helmuthsimon123@gmail.com>
2021-01-05 08:25:50 +0700

    perbaikan

--- resources/views/admin/member-cbt/detail-member1.blade.php
+++ resources/views/admin/member-cbt/detail-member1.blade.php
@@ -3,22 +3,6 @@
 @endsection
 
 @section('content')
-<section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>Detail Anggota CBT</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{route('member')}}">Home</a></li>
-                        <li class="breadcrumb-item"><a href="{{route('member')}}">Anggota CBT</a></li>
-                        <li class="breadcrumb-item active">Detail</li>
-                    </ol>
-                </div>
-            </div>
-        </div><!-- /.container-fluid -->
-    </section>
 
     <section class="content">
         <div class="container-fluid">
